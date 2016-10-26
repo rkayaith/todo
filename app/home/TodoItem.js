b1d9be0ac02c186c29a2c9142c3d6a939de68354
rkayaith <rkayaith@gmail.com>
2016-10-26 18:56:48 -0400

    Added background colors to items based on catagory

--- app/home/TodoItem.js
+++ app/home/TodoItem.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import { View, Text, TextInput, StyleSheet } from 'react-native'
 
-import { style } from '../styles'
+import { style, colors } from '../styles'
 
 export default class TodoItem extends Component {
     render() {
+
+        let backgroundColor = this.props.urgent ?
+            (this.props.important ? colors.red100 : colors.orange100) :
+            (this.props.important ? colors.yellow100 : colors.green100)
+
         return (
-            <View style={ styles.container }>
+            <View style={ [styles.container, { backgroundColor }] }>
                 <Text onPress={ this.onPress } style={ [styles.item, this.props.checked && styles.checked] }>
                     Y
                 </Text>
