a5669ba317c4ba9037202a83658185194f49c528
rkayaith <rkayaith@gmail.com>
2016-11-03 15:26:09 -0400

    Changed scene transitions

--- app/TodoApp.js
+++ app/TodoApp.js
@@ -65,6 +65,7 @@ export default class TodoApp extends Component {
                     }
                     return null
                 }}
+                configureScene={ () => Navigator.SceneConfigs.FloatFromBottomAndroid }
             />
         )
     }
